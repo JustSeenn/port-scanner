fb3d3d737bc29c06275553eed6678fa4574150ec
BOILLOT DEVALLIERE Vincent <vboillot@hotmail.fr>
2022-08-22 12:18:10 +0200

    feat: default port

--- src/scanner.py
+++ src/scanner.py
@@ -1,6 +1,7 @@
 from socket import *;
 import sys
- 
+from IPy import IP
+
 def scan_port(sock, ip, port):
     try:
         sock.connect((ip, port)) 
@@ -9,35 +10,62 @@ def scan_port(sock, ip, port):
     
         return False
 
-def scan_ports_from_ip_detail(ip, range_max, range_min):
+def scan_ports_from_ip(ip, range_max, range_min, detail):
     resultArray = []
-    for i in range(range_min, range_max):
+    for port in range(range_min, range_max):
         sock = socket(AF_INET, SOCK_STREAM)
         sock.settimeout(0.05) 
-        if scan_port(sock, ip, i): 
-            print("[+] Port {} is open".format(i))
-            resultArray.append(i)
-
+        if detail: 
+            if scan_port(sock, ip, port): 
+                print("[+] Port {} is open".format(port))
+                resultArray.append(port)
+            else:
+                print("[-] Port {} is closed".format(port))
         else:
-            print("[-] Port {} is closed".format(i))
+            if scan_port(sock, ip, port): resultArray.append(port)
 
         sock.close()
     return resultArray
 
-def scan_ports_from_ip(ip, range_max, range_min):
-    resultArray = []
-    for i in range(range_min, range_max):
-        sock = socket(AF_INET, SOCK_STREAM)
-        sock.settimeout(0.05)  
-        if scan_port(sock, ip, i): resultArray.append(i)
-        sock.close()
-    return resultArray
 
 def scan_ports_from_name(name, range_max, range_min, detail):
     ip = gethostbyname(name)
-    if detail : return scan_ports_from_ip_detail(ip, range_max, range_min)
-    else : return scan_ports_from_ip(ip, range_max, range_min)
+    return scan_ports_from_ip(ip, range_max, range_min, detail)
 
+def isIP(name):
+    try: 
+        IP(name)
+        return True
+    except ValueError:
+        return False
+
+def default_scan(name, detail):
+    resultArray = []
+    defaultPortList = [20,21,22,23,25,53,137,139,445,80,443,8080,8443,1433,1434,3306,3389]
+    with open('src/defaultPort.txt') as f:
+        lines = f.readlines()
+    if not isIP(name):
+        name = gethostbyname(name)
+
+    for port in defaultPortList:
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.settimeout(0.05) 
+        if detail: 
+            if scan_port(sock, name, port): 
+                print("[+] Port {} is open".format(port))
+                resultArray.append(port)
+            else:
+                print("[-] Port {} is closed".format(port))
+        else:
+            if scan_port(sock, name, port): resultArray.append(port)
+    
+    print('\n[+] Scanning finished\n\n')
+    for port in resultArray:
+        for line in lines:
+            if port == int(line.split(':')[0]):
+                print("[+] Port" + line.split(':')[1].replace('\n','') + " " + str(port) + " is open")
+    
+    sys.exit(0)
 def help():
     print("Usage: python3 scanner.py <ip/url> <port_range_max> <port_range_min>")
     print("\t-u: scan ports from hostname")
@@ -51,22 +79,27 @@ if __name__ == '__main__':
     if '-h' in sys.argv: help()
     if '-u' in sys.argv: url = sys.argv[sys.argv.index('-u') + 1]
     elif '-i' in sys.argv: ip = sys.argv[sys.argv.index('-i') + 1]
+    else : print("[-] Please specify an ip or url")
 
-    if '-min' in sys.argv: min = int(sys.argv[sys.argv.index('-min') + 1]) 
-    else: min = 0
+    if '-d' in sys.argv: detail = True
+    else: detail = False
+    
 
-    if '-max' in sys.argv: max = int(sys.argv[sys.argv.index('-max') + 1])
-    else : max = 100
-    if '-d' in sys.argv: detail = True; 
-    else: detail = False;
+    if '--default' in sys.argv: 
+        if 'url' in locals(): default_scan(url, detail)
+        elif 'ip' in locals(): default_scan(ip, detail)
+    else:
+        if '-min' in sys.argv: min = int(sys.argv[sys.argv.index('-min') + 1]) 
+        else: min = 0
+
+        if '-max' in sys.argv: max = int(sys.argv[sys.argv.index('-max') + 1])
+        else : max = 100
+
     
 
     if 'url' in locals(): result = scan_ports_from_name(url, max, min, detail)
     elif 'ip' in locals(): 
-        if detail :
-            result = scan_ports_from_ip_detail(ip, max, min)
-        else : 
-            result = scan_ports_from_ip(ip, max, min)
+        result = scan_ports_from_ip(ip, max, min, detail)
     else: 
         print('Enter -u or -i')
         sys.exit(0) 
